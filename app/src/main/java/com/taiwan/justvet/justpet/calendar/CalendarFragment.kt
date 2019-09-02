fa73e42c7e0a9c2a4dbfc10e84cf4d76697907c1
eric6300 <6300eric@gmail.com>
2019-09-03 01:17:43 +0800

    implement custom calendar view implement decoration of specific days

--- app/src/main/java/com/taiwan/justvet/justpet/calendar/CalendarFragment.kt
+++ app/src/main/java/com/taiwan/justvet/justpet/calendar/CalendarFragment.kt
@@ -1,15 +1,25 @@
 package com.taiwan.justvet.justpet.calendar
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.prolificinteractive.materialcalendarview.CalendarDay
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener
 import com.taiwan.justvet.justpet.R
 import com.taiwan.justvet.justpet.databinding.FragmentCalendarBinding
+import com.taiwan.justvet.justpet.decorators.EventDecorator
+import com.taiwan.justvet.justpet.home.TAG
+import org.threeten.bp.LocalDate
+import org.threeten.bp.ZoneId
 
-class CalendarFragment : Fragment() {
+class CalendarFragment : Fragment(), OnDateSelectedListener {
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -20,6 +30,28 @@ class CalendarFragment : Fragment() {
             inflater, R.layout.fragment_calendar, container, false
         )
 
+        val instance = LocalDate.now()
+
+        val list = ArrayList<CalendarDay>()
+        list.add(CalendarDay.from(2019,9,27))
+
+        val calendarView = binding.calendarView
+        calendarView.apply {
+            this.setOnDateChangedListener(this@CalendarFragment)
+            this.showOtherDates = MaterialCalendarView.SHOW_ALL
+            this.setSelectedDate(instance)
+            this.addDecorator(EventDecorator(Color.RED, list))
+        }
+
+
         return binding.root
+    }
+
+    override fun onDateSelected(
+        calendarView: MaterialCalendarView,
+        date: CalendarDay,
+        selected: Boolean
+    ) {
+        Log.d(TAG, "selected date : $date")
     }
 }
---
+++ app/src/main/java/com/taiwan/justvet/justpet/decorators/EventDecorator.kt
@@ -0,0 +1,22 @@
+package com.taiwan.justvet.justpet.decorators
+
+import com.prolificinteractive.materialcalendarview.CalendarDay
+import com.prolificinteractive.materialcalendarview.DayViewDecorator
+import com.prolificinteractive.materialcalendarview.DayViewFacade
+import com.prolificinteractive.materialcalendarview.spans.DotSpan
+import java.util.HashSet
+
+/**
+ * Decorate several days with a dot
+ */
+class EventDecorator(private val color: Int, dates: Collection<CalendarDay>) : DayViewDecorator {
+    private val dates: HashSet<CalendarDay> = HashSet(dates)
+
+    override fun shouldDecorate(day: CalendarDay): Boolean {
+        return dates.contains(day)
+    }
+
+    override fun decorate(view: DayViewFacade) {
+        view.addSpan(DotSpan(5f, color))
+    }
+}
