2b43b29e6a98b29e417ca641996e6781aa186a82
eric6300 <6300eric@gmail.com>
2019-09-22 17:16:10 +0800

    implement custom login layout change app icon

--- app/src/main/java/com/taiwan/justvet/justpet/MainActivity.kt
+++ app/src/main/java/com/taiwan/justvet/justpet/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.widget.Toast
@@ -21,6 +22,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.firebase.ui.auth.AuthMethodPickerLayout
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.ErrorCodes
 import com.firebase.ui.auth.IdpResponse
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
@@ -43,6 +45,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private var user: FirebaseUser? = null
     private val viewModel: MainViewModel by lazy {
         ViewModelProviders.of(this).get(MainViewModel::class.java)
     }
@@ -140,7 +143,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         binding.drawerNavView.addHeaderView(bindingNavHeader.root)
     }
 
-    fun setupFAB() {
+    private fun setupFAB() {
         binding.floatingActionButton.setOnClickListener {
             findNavController(R.id.nav_host_fragment).navigate(R.id.navigate_to_tagDialog)
         }
@@ -171,9 +174,16 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     }
 
     override fun onBackPressed() {
-        val drawerLayout = binding.drawerLayout
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START)
+//        val drawerLayout = binding.drawerLayout
+//        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+//            drawerLayout.closeDrawer(GravityCompat.START)
+//        } else {
+//            super.onBackPressed()
+//        }
+
+        if (user == null) {
+            this.finishAndRemoveTask()
+            Log.d(ERIC, "finishAndRemoveTask()")
         } else {
             super.onBackPressed()
         }
@@ -195,11 +205,12 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         if (requestCode == RC_SIGN_IN) {
             if (resultCode != Activity.RESULT_OK) {
                 val response = IdpResponse.fromResultIntent(data)
-                Toast.makeText(
-                    applicationContext,
-                    response?.error?.errorCode.toString(),
-                    Toast.LENGTH_SHORT
-                ).show()
+                finishAndRemoveTask()
+//                Toast.makeText(
+//                    applicationContext,
+//                    response?.error?.errorCode.toString(),
+//                    Toast.LENGTH_SHORT
+//                ).show()
             }
         }
     }
@@ -216,18 +227,21 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         val authListener: FirebaseAuth.AuthStateListener =
             FirebaseAuth.AuthStateListener { auth: FirebaseAuth ->
-                val user: FirebaseUser? = auth.currentUser
+                user = auth.currentUser
                 if (user == null) {
                     val intent = AuthUI.getInstance()
                         .createSignInIntentBuilder()
                         .setAvailableProviders(authProvider)
                         .setAlwaysShowSignInMethodScreen(true)
                         .setIsSmartLockEnabled(false)
+                        .setTheme(R.style.LoginTheme)
                         .setAuthMethodPickerLayout(customLayout)
                         .build()
                     startActivityForResult(intent, RC_SIGN_IN)
                 } else {
-                    UserManager.getFirebaseUser(user)
+                    user?.let {
+                        UserManager.getFirebaseUser(it)
+                    }
                 }
             }
 
